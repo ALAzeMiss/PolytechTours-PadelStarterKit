ab964b2c42413f18e94f0313f79fbe1a8842420a
Amandine L <amandine.langlais03@gmail.com>
2025-12-11 08:49:51 +0100

    fix : not match in admin bar

--- frontend/src/components/NavAdminBar.vue
+++ frontend/src/components/NavAdminBar.vue
@@ -25,13 +25,6 @@
       Poules
     </RouterLink>
 
-    <RouterLink 
-      to="/matches"
-      class="text-lg font-semibold hover:text-blue-600"
-      :class="{ 'text-blue-600 underline' : isActive('/matches') }"
-    >
-      Matchs
-    </RouterLink>
 
     <RouterLink 
       to="/users"
